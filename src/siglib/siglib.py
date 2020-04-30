23b87b17db065141c5a052a54ca6a19c9c769e2f
Michael Recachinas <mgr3yp@virginia.edu>
2020-04-30 21:14:11 +0100

    Run black and format docstring

--- src/siglib/siglib.py
+++ src/siglib/siglib.py
@@ -1,40 +1,51 @@
 #!/usr/bin/env python
-
 import numpy as np
 
 
 def frame(x, frame_length, frame_step, pad=True, pad_value=0j):
     """
-    Creates frames out of the 1D array x that are frame_length 
-                    long, and by stepping frame_step samples.
+    Creates frames out of the 1D array x that are frame_length
+    long, and by stepping frame_step samples.
 
     Parameters
-    -----------
-        x: array of shape (N,) where N is the number of samples
-        frame_length: integer specifying the length of each frame
-        frame_step: integer specifying how many samples to step for each frame
-                        this does not have to be equal to frame_length
-        pad: boolean whether the end of x should be padded before forming the 
-                frames. If false, the frames will only be created out to the
-                last frame that contains samples, if true it will be padded out
-                so that all samples are contained in a frame but the last frame
-                may contain some pad values
-        pad_value: if pad is True, this is the value that should be used to pad
+    ----------
+    x : numpy array of shape (N,) where N is the number of samples
+
+    frame_length : int
+        Length of each frame
+
+    frame_step : int
+        Specifies how many samples to step for each frame
+        this does not have to be equal to frame_length
+
+    pad : bool (optional, default is True)
+        Whether the end of x should be padded before forming the
+        frames. If false, the frames will only be created out to the
+        last frame that contains samples, if true it will be padded out
+        so that all samples are contained in a frame but the last frame
+        may contain some pad values
+
+    pad_value : float or complex (optional, defaults to 0j)
+        If ``pad`` is True, this is the value that should be used to pad
 
     Returns
+    -------
+    x_frame : a (M, frame_length) sized array
+        the framed data
+
+    Examples
     --------
-        x_frame: a (M,frame_length) sized array that contains the framed data 
+    >>>
     """
 
     if pad:
         n_frames = int(np.ceil((x.shape[-1] - frame_length) / frame_step) + 1)
         n_pad = int((n_frames - 1) * frame_step + frame_length - x.shape[-1])
-        x = np.concatenate([x, np.ones(n_pad) * pad_value], axis=-1)
+        x = np.concatenate([x, np.full(n_pad, pad_value)], axis=-1)
     else:
         n_frames = int(np.floor((x.shape[-1] - frame_length) / frame_step) + 1)
     x_frame = np.array(
-        [x[i * frame_step : i * frame_step + frame_length] for i in 
-            range(n_frames)]
+        [x[i * frame_step : i * frame_step + frame_length] for i in range(n_frames)]
     )
     return x_frame
 
@@ -45,15 +56,20 @@ def closing(x, ntaps):
     
     Parameters
     -----------
-        x: array of shape (N,)
-        ntaps: an integer that is the width over which the min max functions
-                are calculated
+    x : array of shape (N,)
+
+    ntaps : int
+        The width over which the min/max functions are calculated
 
     Returns
     --------
-        y: array of shape (N,) that is the closing function over x
-    """
+    y: array of shape (N,)
+        The closing function over x
 
+    Examples
+    --------
+    >>>
+    """
     x = np.concatenate([x, np.ones(ntaps - 1) * x[-1]])
     xf = frame(x, frame_length=ntaps, frame_step=1, pad=False)
     d = np.max(xf, axis=-1)
@@ -69,13 +85,19 @@ def opening(x, ntaps):
     
     Parameters
     -----------
-        x: array of shape (N,)
-        ntaps: an integer that is the width over which the min max functions
-                are calculated
+    x: array of shape (N,)
+
+    ntaps: int
+        The width over which the min max functions  are calculated
 
     Returns
     --------
-        y: array of shape (N,) that is the opening function over x
+    y: array of shape (N,)
+        The opening function over x
+
+    Examples
+    --------
+    >>>
     """
     x = np.concatenate([x, np.ones(ntaps - 1) * x[-1]])
     xf = frame(x, frame_length=ntaps, frame_step=1, pad=False)
@@ -87,6 +109,23 @@ def opening(x, ntaps):
 
 
 def resample(x, idx, ntaps):
+    """
+
+
+    Parameters
+    ----------
+    x: array of shape (N,)
+
+    idx :
+
+    ntaps :
+
+    Returns
+    -------
+
+    Example
+    -------
+    """
     d_idx = np.ceil(np.arange(-ntaps / 2, ntaps / 2))
     idx_array = np.round(idx[:, np.newaxis]) + d_idx
     idx_array = np.asarray(idx_array, dtype=np.int32)
